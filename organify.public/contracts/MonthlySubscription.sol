87c111f281befb3f0b5f181e1fe2e7cce2447d3d
Lucas <lucaspereiracunha08@gmail.com>
2018-08-26 17:00:56 -0700

    working smart-contract

--- organify.public/contracts/MonthlySubscription.sol
+++ organify.public/contracts/MonthlySubscription.sol
@@ -6,20 +6,37 @@ import "./Ownable.sol";
 import "./SafeMath.sol";
 
 contract MonthlySubscription is Whitelist {
-    using SafeMath for uint256;
-    event SubscriptionRejected(address _address);
+   using SafeMath for uint256;
+   mapping (string => uint256) private price;
+    /**
+    * @dev This will set the string we will use for the month + the price it will cost pe month. 
+    */
+   function setPrice(string _role, uint256 _price) public
+   onlyOwner {
+       price[_role] = _price;
+   }
+    /**
+    * @dev This will call the price according to the string pricexmonth
+    */
+   function getPrice(string _role) public
+   view returns (uint256)
+   {
+       return price[_role];
+   }
+    /**
+    * @dev gets msg.value, makes sure its bigger than minimmun for month payment.
+    */
+   function subscribe(string _role) public payable{
+       require(msg.value >= price[_role]);
+       owner.transfer(msg.value);
+       addAddressToWhitelist(msg.sender, _role);
+   }
 
-    function subscribe(string _role) public payable{
-        // user needs to send more than 0.1 eth to successfully deploys the contract and become whitelisted
-        require(msg.value > 10000000000000000);
-        addAddressToWhitelist(msg.sender, _role);
-    }
-
-    function isSubscribed(address _address, string _role) 
-    public onlyIfWhitelisted(_address, _role)
-    view
-    returns (bool){
-        //do something here
-        return true;
-    }
+   function isSubscribed(address _address, string _role)
+   public onlyIfWhitelisted(_address, _role)
+   view
+   returns (bool){
+       //do something here
+       return true;
+   }
 }
